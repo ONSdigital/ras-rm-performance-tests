c0d8f4fa42ca586ab4dfe5b4d5bc00bf2999896b
Matt Crocker <matt.crocker@gmail.com>
2020-05-06 16:24:22 +0100

    Update Helm dependency, pin docker image until chart fixed and proof of concept of data setup

--- _infra/helm/locust/tasks/tasks.py
+++ _infra/helm/locust/tasks/tasks.py
@@ -1,10 +1,17 @@
 from locust import HttpLocust, TaskSet, task, events, between
 import datetime
+import logging
+import sys
+
+# This will only be run on Master and should be used for loading test data
+if '--master' in sys.argv:
+  logger = logging.getLogger()
+  logger.info('This is a setup line')
 
 class ElbTasks(TaskSet):
   @task(1)
   def status(self):
-      response = self.client.get("/sign-in")
+    response = self.client.get("/sign-in")
 
 class ElbWarmer(HttpLocust):
   task_set = ElbTasks
