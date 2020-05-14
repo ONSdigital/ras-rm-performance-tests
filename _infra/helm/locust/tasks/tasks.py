5956f9a48ef4c6b675e5dc7ed77ed02fe9ba7720
Matt Crocker <matt.crocker@gmail.com>
2020-05-14 15:51:13 +0100

    Early data load work

--- _infra/helm/locust/tasks/tasks.py
+++ _infra/helm/locust/tasks/tasks.py
@@ -1,18 +1,17 @@
 from locust import HttpLocust, TaskSet, task, events, between
 import datetime
-import logging
 import sys
+from .dataloader import load_data
 
 # This will only be run on Master and should be used for loading test data
 if '--master' in sys.argv:
-  logger = logging.getLogger()
-  logger.info('This is a setup line')
+  load_data()
 
-class ElbTasks(TaskSet):
+class FrontstageTasks(TaskSet):
   @task(1)
   def status(self):
     response = self.client.get("/sign-in")
 
-class ElbWarmer(HttpLocust):
-  task_set = ElbTasks
+class FrontstageLocust(HttpLocust):
+  task_set = FrontstageTasks
   wait_time = between(5, 15)
