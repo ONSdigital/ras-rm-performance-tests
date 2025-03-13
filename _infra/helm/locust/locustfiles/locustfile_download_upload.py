58d1dcc3697e579f2321744290d7755f1c263c16
SteveScorfield <steve.scorfield@ons.gov.uk>
2025-03-13 11:00:12 +0000

    Have update the max/min times to ints not strings

--- _infra/helm/locust/locustfiles/locustfile_download_upload.py
+++ _infra/helm/locust/locustfiles/locustfile_download_upload.py
@@ -36,8 +36,8 @@ auth = (os.getenv('security_user_name'), os.getenv('security_user_password'))
 ignore_columns = ['surveyRef', 'exerciseRef']
 CSRF_REGEX = re.compile(r'<input id="csrf_token" name="csrf_token" type="hidden" value="(.+?)"\/?>')
 # USER_WAIT_TIME_WAIT_TIME is between GET and POST requests
-USER_WAIT_TIME_MIN_SECONDS = f"{os.getenv('user_wait_time_min_seconds')}"
-USER_WAIT_TIME_MAX_SECONDS = f"{os.getenv('user_wait_time_max_seconds')}"
+USER_WAIT_TIME_MIN_SECONDS = int(os.getenv('user_wait_time_min_seconds'))
+USER_WAIT_TIME_MAX_SECONDS = int(os.getenv('user_wait_time_max_seconds'))
 
 
 # Load data for tests
